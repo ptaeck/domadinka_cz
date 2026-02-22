707dd38ee259b53ecbcb0cebaf3956d59a9c526b
ptaeck <ptaeck@gmail.com>
2026-02-22 18:35:48 +0100

    Update vite.config.ts
    custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  server: { host: "::", port: 8080 },
+  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  base: '/'  // Use '/' when serving from a custom domain
 }));
+
